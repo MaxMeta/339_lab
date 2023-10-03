7792dba1c33c472b9ea71fe38aec2f4380f0a2ff
MaxMeta <jeremy.owen@vuw.ac.nz>
2023-10-03 18:56:20 +1300

    Add files via upload

---
+++ install_packages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+conda create -n biol339 python=3.9 -y
+conda activate biol339
+conda install -c bioconda spades -y
+pip install jupyterlab
+pip install bash_kernel
+python -m bash_kernel.install
+conda install -c bioconda kallisto -y
+conda install -c bioconda glimmer -y
+pip install pydeseq2
+conda install -c bioconda adapterremoval -y
+echo Done!
